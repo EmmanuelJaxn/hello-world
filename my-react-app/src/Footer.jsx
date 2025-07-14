77441f03fbceb68ad22787c57645fdca81fbdd8b
EmmanuelJaxn <angelmichaelz@gmail.com>
2025-07-13 18:38:49 -0800

    first commit

---
+++ my-react-app/src/Footer.jsx
@@ -0,0 +1,11 @@
+
+function Footer(){
+
+  return(
+    <footer>
+      <p>&copy; {new Date().getFullYear()} Emmanuel Jackson</p>
+    </footer>
+  );
+}
+
+export default Footer
---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,12 @@
+
+function Header(){
+
+  return(
+    <header>
+      <h1>Hello World!</h1>
+      <hr></hr>
+    </header>
+  );
+}
+
+export default Header
